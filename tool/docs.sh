dd2800bda863a3ec6b4cda37268fef4d6bd48382
George Lesica <george@lesica.com>
2019-05-16 21:03:52 -0600

    Update docs and bump version

--- tool/docs.sh
+++ tool/docs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Generate example outputs
-pub run dcdg -o example/dcdg.puml
+pub run dcdg -o example/dcdg.puml --exclude-private=field,method
 plantuml example/dcdg.puml
 
 # Add the --help contents to a text file
