b00f7c39ca3f20d725350ecf9093bf3f5867dd90
George Lesica <george@lesica.com>
2021-10-19 12:43:15 -0600

    Update for new analyzer version

--- lib/src/type_name.dart
+++ lib/src/type_name.dart
@@ -3,6 +3,9 @@ import 'package:analyzer/dart/element/element.dart';
 /// Determine and return the most sensible "name" for the given
 /// [Element].
 String typeName(final Element element, {bool withNullability = true}) {
+  // If we're building a has-a relationship then we get the
+  // field that induces the relationship and we have to do a
+  // little extra work to get the type.
   if (element is FieldElement) {
     return element.type.getDisplayString(withNullability: withNullability);
   }
